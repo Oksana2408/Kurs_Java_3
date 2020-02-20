1dc4e447c8f501a459aa24fde316f4fee9004afd
Oksana2408 <oksanamk@gmx.de>
2020-02-20 07:53:50 +0100

    ДЗ к 5 лекции 3 курса Java

---
+++ Car.java
@@ -0,0 +1,45 @@
+package Lesson_5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable{
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+
+    CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник # " + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+       try{
+           System.out.println(this.name + "готовиться");
+           Thread.sleep(500 + (int)(Math.random() * 800));
+           cyclicBarrier.await();
+           System.out.println(this.name + "готов!");
+           cyclicBarrier.await();
+       } catch (Exception e){
+           e.printStackTrace();
+       }
+
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+    }
+}
---
+++ Road.java
@@ -0,0 +1,26 @@
+package Lesson_5;
+
+import java.util.concurrent.Semaphore;
+
+public class Road extends Stage {
+    final Semaphore smp = new Semaphore(2);
+
+    public Road(int length){
+        this.length = length;
+        this.description = "Дорога" + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try{
+            smp.acquire();
+            System.out.println(c.getName()+ " начал этап: " + description);
+            Thread.sleep(length/c.getSpeed() * 1000);
+            System.out.println(c.getName() + " закончил этап: " + description);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            smp.release();
+        }
+    }
+}
